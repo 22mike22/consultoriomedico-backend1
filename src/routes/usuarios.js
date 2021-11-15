7c89543693214ee64842e3ac47b5519e8d1a81d0
22mike22 <miguel_222220@hotmail.com>
2021-11-15 14:49:51 -0700

    actualizaciones de dependencias

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+    //res.send({message:'hola mundo'})
   res.send('You have hit GET /usuarios endpoint')
 })
 
